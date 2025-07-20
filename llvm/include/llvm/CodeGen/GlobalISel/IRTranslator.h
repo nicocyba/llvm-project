dc743f81d1ee89edc2b358bdea888678db221711
Nico Cyba <nicocyba@gmail.com>
2025-07-20 23:03:37 +0200

    u

--- llvm/include/llvm/CodeGen/GlobalISel/IRTranslator.h
+++ llvm/include/llvm/CodeGen/GlobalISel/IRTranslator.h
@@ -28,6 +28,7 @@
 #include "llvm/CodeGen/SwitchLoweringUtils.h"
 #include "llvm/Support/Allocator.h"
 #include "llvm/Support/CodeGen.h"
+#include "llvm/ADT/Statistic.h"
 #include <memory>
 #include <utility>
 
