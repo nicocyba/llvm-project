dd082d308f69870deba1a6aec867456f25957bb7
Nico Cyba <nicocyba@gmail.com>
2025-07-20 16:01:13 +0200

    improve dataformat

--- llvm/lib/CodeGen/MachineCombiner.cpp
+++ llvm/lib/CodeGen/MachineCombiner.cpp
@@ -445,7 +445,7 @@ insertDeleteInstructions(MachineBasicBlock* MBB, MachineInstr& MI, SmallVectorIm
     // Otherwise the constant pool entry created for InsInstrs will not be deleted
     // even if InsInstrs is not the better pattern.
     unsigned idx = std::distance(MBB->begin(), MachineBasicBlock::iterator(&MI));
-    llvm::outs() << __FILE__ << " | " << __func__ << "| BB: " << MBB->getName() << " | idx: " << idx << "\n";
+    // llvm::outs() << __FILE__ << " | " << __func__ << "| BB: " << MBB->getName() << " | idx: " << idx << "\n";
     
     TII->finalizeInsInstrs(MI, Pattern, InsInstrs);
     
@@ -467,6 +467,7 @@ insertDeleteInstructions(MachineBasicBlock* MBB, MachineInstr& MI, SmallVectorIm
         llvm::raw_string_ostream rso(instrStr);
         InstrPtr->print(rso);
         rso.flush();
+        instrStr = std::regex_replace(instrStr, std::regex("\\n"), "");
         data.inserted.push_back(instrStr);
     }
 
@@ -476,7 +477,7 @@ insertDeleteInstructions(MachineBasicBlock* MBB, MachineInstr& MI, SmallVectorIm
         InstrPtr->print(rso);
         rso.flush();
         idx = std::distance(MBB->begin(), MachineBasicBlock::iterator(InstrPtr));
-        instrStr = std::regex_replace(instrStr, std::regex("\n"), "");
+        instrStr = std::regex_replace(instrStr, std::regex("\\n"), "");
         instrStr += " | idx=" + std::to_string(idx);
         data.deleted.push_back(instrStr);
 
