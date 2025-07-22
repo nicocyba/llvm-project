9b1bdfb2539587cdb30c3aa5379f81c0eef49698
Nico Cyba <nicocyba@gmail.com>
2025-07-22 19:23:30 +0200

    bf

--- llvm/include/llvm/CodeGen/GlobalISel/MIPatternMatch.h
+++ llvm/include/llvm/CodeGen/GlobalISel/MIPatternMatch.h
@@ -23,15 +23,12 @@ namespace MIPatternMatch {
 
 template <typename... Args>
 inline void logPattern(Args &&...args) {
-  llvm::outs() << "\tPattern used: " << args << "\n";
+  ((llvm::outs() << "\tPattern used: " << args << "\n"), ...);
 }
 
 template <typename Reg, typename Pattern>
 [[nodiscard]] bool mi_match(Reg R, const MachineRegisterInfo &MRI, Pattern &&P) {
   llvm::outs() << "mi_match with reg: " << R << "\n";
-  if (P.hasName()) {
-    logPattern(P.getName(), P.getKind());
-  }
   return P.match(MRI, R);
 }
 
@@ -39,9 +36,6 @@ template <typename Pattern>
 [[nodiscard]] bool mi_match(MachineInstr &MI, const MachineRegisterInfo &MRI,
                             Pattern &&P) {
   llvm::outs() << "mi_match: " << MI << "\n";
-  if (P.hasName()) {
-    logPattern(P.getName(), P.getKind());
-  }
   return P.match(MRI, &MI);
 }
 
