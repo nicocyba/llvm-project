dec25df3d75be2fe96f35dc06f61411b520dcb80
Nico Cyba <nicocyba@gmail.com>
2025-07-23 23:14:57 +0200

    fix: comment out AArch64 subdirectory to prevent CMake directory conflict and update include path for AArch64InstrInfo

--- llvm/lib/CodeGen/MachineCombiner.cpp
+++ llvm/lib/CodeGen/MachineCombiner.cpp
@@ -36,7 +36,7 @@
 // #include "llvm/Support/TargetSelect.h"
 // #include "AArch64Subtarget.h"
 // #include "AArch64TargetMachine.h"
-#include "llvm/lib/Target/AArch64/AArch64InstrInfo.h"
+#include "lib/Target/AArch64/AArch64InstrInfo.h"
 
 #include <regex>
 using namespace llvm;
