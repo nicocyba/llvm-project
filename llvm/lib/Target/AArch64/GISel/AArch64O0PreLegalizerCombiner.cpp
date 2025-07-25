0a11643589092726218933a882609bb197b4125e
Nico Cyba <nicocyba@gmail.com>
2025-07-25 10:09:46 +0200

    fix: improve debug output in AArch64O0PreLegalizerCombiner by correcting syntax

--- llvm/lib/Target/AArch64/GISel/AArch64O0PreLegalizerCombiner.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64O0PreLegalizerCombiner.cpp
@@ -79,7 +79,7 @@ AArch64O0PreLegalizerCombinerImpl::AArch64O0PreLegalizerCombinerImpl(
 
 bool AArch64O0PreLegalizerCombinerImpl::tryCombineAll(MachineInstr& MI) const {
     outs() << "\t" << __PRETTY_FUNCTION__ << " - "
-           << "MI: " << MI << "\n";>>
+           << "MI: " << MI << "\n";
     if (tryCombineAllImpl(MI)) {
         return true;
     }
