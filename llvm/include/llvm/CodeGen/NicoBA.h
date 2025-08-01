575678bc05d980e32bfcdb7045bcec0197403831
Nico Cyba <nicocyba@gmail.com>
2025-08-01 16:17:31 +0200

    fix: update MachineInstr references to use llvm namespace in NicoBA.h

--- llvm/include/llvm/CodeGen/NicoBA.h
+++ llvm/include/llvm/CodeGen/NicoBA.h
@@ -437,9 +437,9 @@ inline std::string to_string(AArch64MachineCombinerPattern2 pattern) {
 }
 
 
-inline thread_local std::vector<const MachineInstr *> CreatedInstrsNico;
-inline thread_local std::vector<const MachineInstr *> DeletedInstrsNico;
-inline thread_local std::vector<const MachineInstr *> ChangedInstrsNico;
+inline thread_local std::vector<const llvm::MachineInstr *> CreatedInstrsNico;
+inline thread_local std::vector<const llvm::MachineInstr *> DeletedInstrsNico;
+inline thread_local std::vector<const llvm::MachineInstr *> ChangedInstrsNico;
 
 inline void reset_observerdata() {
     CreatedInstrsNico.clear();
