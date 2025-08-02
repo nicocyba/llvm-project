87f6aa39696f724b6cfb53ff6bedb1faaafc1ed3
Nico Cyba <nicocyba@gmail.com>
2025-08-02 22:25:00 +0200

    fix: correct formatv call in GlobalISelCombinerEmitter for proper string formatting

--- llvm/utils/TableGen/GlobalISelCombinerEmitter.cpp
+++ llvm/utils/TableGen/GlobalISelCombinerEmitter.cpp
@@ -1950,7 +1950,7 @@ for (const auto &C : nico::ChangedInstrsNico)
 if (!obs_changed.empty() && obs_changed.size() >= 3)
     obs_changed.erase(obs_changed.size() - 3);
 
-std::string obs_deleted = formatv("{{0}}", nico::DeletedInstrsNico.size());
+std::string obs_deleted = formatv("{{0}", nico::DeletedInstrsNico.size());
 
 std::string temp_after;
 for (const auto &C : State.MIs)
