aa6433fa7e8f34bd4c71a702c75354b6a354fdcd
Nico Cyba <nicocyba@gmail.com>
2025-08-02 20:54:29 +0200

    fix: make getUnixTimestampStringChrono inline for better optimization

--- llvm/include/llvm/CodeGen/NicoBA.h
+++ llvm/include/llvm/CodeGen/NicoBA.h
@@ -11,7 +11,7 @@
 #include <chrono>
 
 namespace nico {
-std::string getUnixTimestampStringChrono() {
+inline std::string getUnixTimestampStringChrono() {
     auto currentTimePoint = std::chrono::system_clock::now();
     auto durationSinceEpoch = currentTimePoint.time_since_epoch();
     auto secondsSinceEpoch = std::chrono::duration_cast<std::chrono::seconds>(durationSinceEpoch);
