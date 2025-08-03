3925d415d2b2aabd62953bcacb22b791cd5da20a
Nico Cyba <nicocyba@gmail.com>
2025-08-03 17:15:33 +0200

    fix: consolidate GlobalISel data structures and improve MI2String function for better clarity and organization

--- llvm/include/llvm/CodeGen/NicoBA.h
+++ llvm/include/llvm/CodeGen/NicoBA.h
@@ -11,6 +11,49 @@
 #include <chrono>
 
 namespace nico {
+struct GlobalISelData {
+    std::string caller; // irtranslator, legalizer, ...
+    std::string event; // created, deleted, special
+    std::string mf; // mf name
+    std::string mbb; // mbb name
+    std::string mi_before; // mi name
+    std::string mi_after; // mi name
+    std::string pattern; // MIPattern
+};
+
+struct GlobalISelDataPattern {
+    std::string stage; // irtranslator, legalizer, ...
+    std::string pattern_match_file;
+    std::string pattern_match_name;
+    std::string pattern_match_type; 
+    std::string mbb; // mbb name
+    bool match_success;
+};
+
+// definition of machinecombiner data structure
+struct MachineCombinerData {
+    unsigned idx;
+    unsigned mbb_pred;
+    unsigned mbb_succ;
+    unsigned mbb_size;
+    unsigned mf_size;
+    std::vector<std::string> inserted;
+    std::vector<std::string> deleted;
+    std::string mf;
+    std::string mbb;
+    std::string pattern;
+};
+
+auto MI2String = [](const MachineInstr& MI) {
+    std::string InstrStr;
+    llvm::raw_string_ostream OS(InstrStr);
+    MI.print(OS);
+    OS.flush();
+    InstrStr = std::regex_replace(InstrStr, std::regex("\\n"), "");
+    InstrStr = std::regex_replace(InstrStr, std::regex("<regmask.*more...>"), "<regmask...>");
+    return InstrStr;
+};
+
 inline std::string to_string(int value) {
     switch (value) {
         case 0: return "GIM_Try";
@@ -620,52 +663,6 @@ inline void reset_observerdata(const std::string& filename, const std::string& f
     ChangedInstrsNico.clear();
 }
 
-} // end namespace nico
-
-namespace llvm {
-struct GlobalISelData {
-    std::string caller; // irtranslator, legalizer, ...
-    std::string event; // created, deleted, special
-    std::string mf; // mf name
-    std::string mbb; // mbb name
-    std::string mi_before; // mi name
-    std::string mi_after; // mi name
-    std::string pattern; // MIPattern
-};
-
-struct GlobalISelDataPattern {
-    std::string stage; // irtranslator, legalizer, ...
-    std::string pattern_match_file;
-    std::string pattern_match_name;
-    std::string pattern_match_type; 
-    std::string mbb; // mbb name
-    bool match_success;
-};
-
-// definition of machinecombiner data structure
-struct MachineCombinerData {
-    unsigned idx;
-    unsigned mbb_pred;
-    unsigned mbb_succ;
-    unsigned mbb_size;
-    unsigned mf_size;
-    std::vector<std::string> inserted;
-    std::vector<std::string> deleted;
-    std::string mf;
-    std::string mbb;
-    std::string pattern;
-};
-
-auto MI2String = [](const MachineInstr& MI) {
-    std::string InstrStr;
-    llvm::raw_string_ostream OS(InstrStr);
-    MI.print(OS);
-    OS.flush();
-    InstrStr = std::regex_replace(InstrStr, std::regex("\\n"), "");
-    InstrStr = std::regex_replace(InstrStr, std::regex("<regmask.*more...>"), "<regmask...>");
-    return InstrStr;
-};
-
 auto log_backend_event = [](auto&&... args) {
     data_globalisel_patterns.emplace_back(GlobalISelDataPattern{std::forward<decltype(args)>(args)...});
 };
@@ -758,7 +755,6 @@ inline thread_local GlobalISelDataVector<GlobalISelDataPattern> data_globalisel_
 
 inline thread_local std::vector<std::tuple<const std::string, const std::string, unsigned>> data_gicombiner;
 
-} // end namespace llvm...
 
 
 inline std::optional<std::string> extractT3Type(const std::string& input) {
@@ -871,3 +867,6 @@ auto escapeString = [](const std::string& input) -> std::string {
         }
         return out;
     };
+
+
+} // end namespace nico
