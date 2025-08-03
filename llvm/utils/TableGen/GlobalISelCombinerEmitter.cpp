b1298dfa47d94f4faffdacf5b655d239a7c66747
Nico Cyba <nicocyba@gmail.com>
2025-08-03 19:56:21 +0200

    fix: correct function name from reset_observerdata_failure to reset_observerdata_failed and update parameters in reset_observerdata_success

--- llvm/utils/TableGen/GlobalISelCombinerEmitter.cpp
+++ llvm/utils/TableGen/GlobalISelCombinerEmitter.cpp
@@ -1921,7 +1921,7 @@ OS << formatv(content0.c_str(), RuleID, RuleDef.getName());
             Expander.emit(OS);
             OS << "}()) {\n"
                << "  outs() << \"\\t\\t\\t\\t\\t\\t-> Match failed\\n\";\n"
-               << "  nico::reset_observerdata_failure(__FILE__, __FUNCTION__, \"" << RuleDef.getName() << "\", " << RuleID << ");\n"
+               << "  nico::reset_observerdata_failed(__FILE__, __FUNCTION__, \"" << RuleDef.getName() << "\", " << RuleID << ");\n"
                << "  return false;\n}\n";
             CodeStrNico += M->getRawCode().str() + " | ";
         }
@@ -1951,11 +1951,10 @@ OS << content1;
 
     // NICO
     std::string content = R"(
-// Nico
 std::vector<std::tuple<std::string, unsigned, unsigned>> temp_after;
 for (const auto& C : State.MIs) { temp_after.push_back(std::make_tuple(nico::MI2String(*C), -1, -1)); }
 
-nico::reset_observerdata_success(__FILE__, __FUNCTION__, temp_before, temp_after, {0}, {1});
+nico::reset_observerdata_success(__FILE__, __FUNCTION__, temp_before, temp_after, "{0}", {1});
 )";
 
     OS << formatv(content.c_str(), RuleDef.getName(), RuleID);
