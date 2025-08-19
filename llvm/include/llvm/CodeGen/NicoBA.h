33ed36bb5a75d8c38419ac2752fff670b3e83a1d
Nico Cyba <nicocyba@gmail.com>
2025-08-19 22:15:42 +0200

    fix: comment out debug output for rule ID logging in GIMatchTableExecutor and NicoBA

--- llvm/include/llvm/CodeGen/NicoBA.h
+++ llvm/include/llvm/CodeGen/NicoBA.h
@@ -266,8 +266,8 @@ inline void reset_observerdata_success(const std::string& filename, const std::s
     // total_data.back().state_after = std::move(state_after_loc);
     unsigned ruleid;
     if (total_data.back().rules.empty()) {
-        ruleid = 0;
-        llvm::outs() << "reset_observerdata_success - rules empty --> stage: " << current_stage << "\n";
+        ruleid = 99999;
+        // llvm::outs() << "reset_observerdata_success - rules empty --> stage: " << current_stage << "\n";
     } else {
         ruleid = std::get<1>(total_data.back().rules.back());
     }
